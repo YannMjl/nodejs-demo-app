02184d5ba48f25234463141fce4c3fee497d1c31
Yann Mulonda <yann.mulonda@icfnext.com>
2021-06-19 08:31:21 -0500

    added terraform

---
+++ main.tf
@@ -0,0 +1,31 @@
+# -------------------------------------------------------------*
+# Set the required provider and versions
+# -------------------------------------------------------------*
+terraform {
+  required_providers {
+    # it's now recommend pinning to the specific version of the 
+    # Docker Provider you're using since new versions are 
+    # released frequently
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.12.1"
+    }
+  }
+}
+
+# Configure the docker provider
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
